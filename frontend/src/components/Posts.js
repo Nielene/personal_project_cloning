d07d732622e50f259dacdf42cd908a6a68f9ef67
Nielene Thomas <nielenethomas@pursuit.org>
2019-02-25 12:42:38 -0500

    posts render!

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class Posts extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    }
+  }
 
   componentDidMount() {
     axios.get('http://jsonplaceholder.typicode.com/posts')
-    .then(res => console.log(res.data))
+    .then(res => {
+      console.log(res.data);
+      this.setState({posts: res.data})
+    })
     // fetch('http://jsonplaceholder.typicode.com/posts')
     // .then(res => res.json())
     // .then(data => console.log(data))
   }
 
   render () {
-    // console.log('data');
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ))
     return (
       <div>
         <h1>Posts</h1>
+        {postItems}
       </div>
     )
   }
