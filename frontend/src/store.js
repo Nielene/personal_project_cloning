7581621871cb23a783babcffc286b4e0c495b343
Nielene Thomas <nielenethomas@pursuit.org>
2019-02-26 10:51:39 -0500

    reed removed "initialState" from store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 //i changed the routing in './index.js'
 import rootReducer from './reducers/index.js'
-//i added line 5:
-import initialState from './reducers/index.js'
 
 
 const intialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
